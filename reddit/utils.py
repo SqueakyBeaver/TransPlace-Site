e6aad25a391be06232eb0f803ccccbbb3d21e993
SqueakyBeaver <theboxguy659@gmail.com>
2023-05-17 20:54:23 -0500

    Commit before I nuke server messages support; allow dynamically fetching Reddit posts and putting them in cache

---
+++ reddit/utils.py
@@ -0,0 +1,60 @@
+from dotenv import load_dotenv
+from os import getenv
+from django.core.cache import cache
+import praw
+# import pprint
+
+
+def get_messages(count=15):
+    """
+    Get specified messages from the reddit
+    :param count: The total amount of messages to get
+    :type amount: int
+    """
+
+    load_dotenv()
+
+    reddit = praw.Reddit(
+        client_id=getenv("REDDIT_CLIENT_ID"),
+        client_secret=getenv("REDDIT_CLIENT_SECRET"),
+        refresh_token=getenv("REDDIT_REFRESH_TOKEN"),
+        user_agent=getenv("REDDIT_USER_AGENT"),
+    )
+
+    result = []
+
+    subreddit: praw.reddit.Subreddit = reddit.subreddit("transplace")
+    for submission in subreddit.new(limit=count):
+        # In case I ever need to figure out
+        # all the properties a submission has
+        # pprint.pprint(vars(submission))
+
+        if submission.archived:
+            print(submission.title, "ARCHIVED")
+            continue
+
+        data = {
+            "id": submission.id,
+            "title": submission.title,
+            "content": None,
+            "message_type": "User Content",
+            "reddit_url": submission.shortlink,
+        }
+
+        # If the post is an image post
+        # We have to do it this way bc PRAW does funny stuff
+        try:
+
+            if submission.preview["enabled"]:
+                data["file_url"] = submission.url
+
+            if "video" in submission.post_hint:
+                data["file_url"] = submission.preview["images"][0]["resolutions"][-1]["url"]
+
+        except BaseException as e:
+            pass
+
+        result.append(data)
+
+    # 🙏
+    cache.set("messages", result)
